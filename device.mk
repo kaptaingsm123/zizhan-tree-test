e5444246c4a19168eb5204bb7554cf154b2963e8
kaptaingsm123 <154521627+kaptaingsm123@users.noreply.github.com>
2024-01-03 23:43:32 +0200

    zizhan

--- device.mk
+++ device.mk
@@ -7,36 +7,17 @@
 
 LOCAL_PATH := device/xiaomi/zizhan
 # A/B
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    gz \
-    lk \
-    logo \
-    md1img \
-    preloader \
-    product \
-    scp \
-    spmfw \
-    sspm \
-    system \
-    system_ext \
-    tee \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor \
-    vendor_boot
-
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
-# Virtual A/B
-ENABLE_VIRTUAL_AB := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
@@ -118,9 +99,6 @@ BOARD_SYSTEMSDK_VERSIONS := $(BOARD_SHIPPING_API_LEVEL)
 PRODUCT_PACKAGES += fastbootd
 PRODUCT_PACKAGES += android.hardware.fastboot@1.1-impl-mock
 
-PRODUCT_PACKAGES += \
-    bootctrl.taro
-
 PRODUCT_SHARED_BOOT_CONTROL_HAL := \
     bootctrl.taro \
     libgptutils \
@@ -133,3 +111,7 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_verifier \
     update_engine_sideload
+
+# VNDK/SDK
+PRODUCT_TARGET_VNDK_VERSION := $(BOARD_SHIPPING_API_LEVEL)
+BOARD_SYSTEMSDK_VERSIONS := $(BOARD_SHIPPING_API_LEVEL)
